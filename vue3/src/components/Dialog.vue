66f2b0619ae78c4bcd860cafe000d0e0f69280ca
wu_yiqiang <sutter.wu@itforce-tech.com>
2024-11-27 11:33:13 +0800

    添加dialog和购物车动画组件

---
+++ vue3/src/components/Dialog.vue
@@ -0,0 +1,72 @@
+<template>
+    <Teleport v-if="open" to="body">
+        <dialog class="CustomDialog" @keydown="(event) => handleEsc(event)">
+            <div v-if="closable" class="close" @click="open = false">
+                <SvgIcon name="off" size="20px" />
+            </div>
+            <div class="contents">
+                <slot />
+            </div>
+        </dialog>
+    </Teleport>
+</template>
+
+<script setup lang='ts'>
+import ImagePreview from './AuthImage/ImagePreview.vue';
+import SvgIcon from '@/components/SvgIcon/index.vue'
+import { isArlang } from '@/hooks/index';
+const emit = defineEmits(['update:visible']);
+const props = defineProps({
+    visible: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    closable: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+});
+const open = computed({
+    get() {
+        return props.visible;
+    },
+    set(value) {
+        emit('update:visible', value);
+    },
+});
+const handleEsc = (e: Event) => {
+    if (e?.keyCode == 27) open.value = false
+}
+</script>
+<style scoped lang='scss'>
+.CustomDialog {
+    background-color: rgba(102, 128, 158, .7);
+    position: sticky;
+    transform: translate(0,-100%);
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    @include hor-ver-align-grid();
+    display: flex;
+    .close {
+        position: fixed;
+        right: 36px;
+        top:20px;
+        z-index: 9999999;
+        width: 48px;
+        aspect-ratio: 1 / 1;
+        border-radius: 50%;
+        background-color: rgba(19, 18, 17, .7);
+        @include hor-ver-align-grid();
+        &:hover {
+            background-color: rgba(19, 18, 17, 1);
+        }
+    }
+    .contents {
+       flex: 1;
+        @include hor-ver-align-grid();
+    }
+}
+</style>
